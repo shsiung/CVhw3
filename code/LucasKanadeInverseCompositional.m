74c393083f3d9811428e811fa5d12b9be3862a47
Jerry Hsiung <shsiung@g.hmc.edu>
2016-10-20 02:30:00 -0400

    maybe working section 1

--- code/LucasKanadeInverseCompositional.m
+++ code/LucasKanadeInverseCompositional.m
@@ -12,8 +12,8 @@ init_dwdp = @(x,y) [0 0 0 0 1 0; 0 0 0 0 0 1];
 [Gx, Gy] = imgradientxy(It,'intermediate');
 whole_rect = round(rect);
 
-Gx = imtranslate(Gx,-[whole_rect(1)-rect(1),rect(2)-whole_rect(2)]);
-Gy = imtranslate(Gy,-[whole_rect(1)-rect(1),rect(2)-whole_rect(2)]);
+Gx = imtranslate(Gx,[whole_rect(1)-rect(1),rect(2)-whole_rect(2)]);
+Gy = imtranslate(Gy,[whole_rect(1)-rect(1),rect(2)-whole_rect(2)]);
 Gx = Gx(whole_rect(2):whole_rect(4), whole_rect(1):whole_rect(3));
 Gy = Gy(whole_rect(2):whole_rect(4), whole_rect(1):whole_rect(3));
 
@@ -36,9 +36,9 @@ p4 = 0;
 p5 = 0;
 p6 = 0; %v
 
-epsilon = 0.01;
+epsilon = 0.4;
 p = [1+p1 p3 p5; p2 1+p4 p6];
-temp_It = imtranslate(It,-[whole_rect(1)-rect(1),rect(2)-whole_rect(2)]);
+temp_It = imtranslate(It,[whole_rect(1)-rect(1),rect(2)-whole_rect(2)]);
 temp_It = temp_It(whole_rect(2):whole_rect(4), whole_rect(1):whole_rect(3));
 dP = p;
 newP = p;
@@ -53,13 +53,12 @@ newP = p;
 
 while norm(dP) > epsilon
     % Warp image according to warped window
-    warp_It1 = imtranslate(It1,[p5,p6]);
+    warp_It1 = imtranslate(It1,[-newP(5),-newP(6)]);
 %     warp_It1 = interp2(xx,yy,It1,xx+newP(1,3),yy+newP(2,3));
     warp_It1 = warp_It1(whole_rect(2):whole_rect(4), whole_rect(1):whole_rect(3));
     % Compute error by subtracting It1 window with I;t window
     error_image = warp_It1 - temp_It;
     
-    % imshowpair(temp_It,error_image,'montage');
     % Per pixel, multiply steepest descent
     SDparam = zeros(6,1);
 
@@ -80,20 +79,23 @@ while norm(dP) > epsilon
     p4 = p4+dP(4)+p2*dP(3)+p4*dP(4);
     p5 = p5+dP(5)+p1*dP(5)+p3*dP(6);
     p6 = p6+dP(6)+p2*dP(5)+p4*dP(6);
-%      newP = 1/((1+p1)*(1+p4)-p2*p3)*[-p1-p1*p4+p2*p3; ...
-%                                             -p2;...
-%                                             -p3;...
-%                                           -p4-p1*p4+p2*p3;...
-%                                           -p5-p4*p5+p3*p6;...
-%                                           -p6-p1*p6+p2*p5];
-%    
-    %rec_corner = arrayfun(@(x,p) W(x,newp), rec_corner, newP);
-    %newRec = [rec_corner(1,1), rec_corner(1,2), rec_corner(4,1), rec_corner(4,2)];
-end
-% subplot(2,2,4);
+    newP = 1/((1+p1)*(1+p4)-p2*p3)*[-p1-p1*p4+p2*p3; ...
+                                            -p2;...
+                                            -p3;...
+                                          -p4-p1*p4+p2*p3;...
+                                          -p5-p4*p5+p3*p6;...
+                                          -p6-p1*p6+p2*p5];
+%     norm(dP)
+%     subplot(2,2,4);
 %     imshow(warp_It1);
 %     subplot(2,2,3);
 %     imshow(error_image);
 %     drawnow;
-u = p5;
-v = p6;
+
+    %rec_corner = arrayfun(@(x,p) W(x,newp), rec_corner, newP);
+    %newRec = [rec_corner(1,1), rec_corner(1,2), rec_corner(4,1), rec_corner(4,2)];
+end
+
+
+u = newP(5);
+v = newP(6);
--- code/testCarSequence.m
+++ code/testCarSequence.m
@@ -10,5 +10,5 @@ for i = 1 : size(data,3)-1
    rectangle('Position',[rect(1),rect(2),rect(3)-rect(1),rect(4)-rect(2)]);
    drawnow;
    [u, v] = LucasKanadeInverseCompositional(data(:,:,i),data(:,:,i+1),rect);
-   rect = [rect(1)-u, rect(2)-v, rect(3)-u, rect(4)-v];
+   rect = [rect(1)+u, rect(2)+v, rect(3)+u, rect(4)+v];
 end
