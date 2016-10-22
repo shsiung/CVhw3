c62f1fb4abd5c16296b8d8b0e8eb6a86931cf46f
shsiung <shsiung@g.hmc.edu>
2016-10-21 20:05:08 -0400

    working on affine

--- code/LucasKanadeAffine.m
+++ code/LucasKanadeAffine.m
@@ -1,36 +1,29 @@
 function M = LucasKanadeAffine(It, It1)
-
 % input - image at time t, image at t+1 
 % output - M affine transformation matrix
-
-
-% input - image at time t, image at t+1, rectangle (top left, bot right coordinates)
-% output - movement vector, [u,v] in the x- and y-directions.
 
 It = im2double(It);
 It1 = im2double(It1);
 %% Precomputation
 
-% Template gradient
-[Gx, Gy] = imgradientxy(It,'intermediate');
-whole_rect = round(rect);
-Gx = imtranslate(Gx,[whole_rect(1)-rect(1),whole_rect(2)-rect(2)]);
-Gy = imtranslate(Gy,[whole_rect(1)-rect(1),whole_rect(2)-rect(2)]);
-Gx = Gx(whole_rect(2):whole_rect(4), whole_rect(1):whole_rect(3));
-Gy = Gy(whole_rect(2):whole_rect(4), whole_rect(1):whole_rect(3));
+% Template gradient - find overlapped region
 
-% Steepest Descent
-% Jacobian
-% init_dwdp =  [0 0 0 0 1 0; 0 0 0 0 0 1];
+temp_It = It;
+It_mask = (temp_It~=0);
+
+[Gx,Gy] = imgradientxy(temp_It);
+
+init_dwdp =@(x,y)  [x 0 y 0 1 0; 0 x 0 y 0 1]; % Jacobian
 steepest_desc = zeros(whole_rect(4)-whole_rect(2)+1,whole_rect(3)-whole_rect(1)+1,6);
-steepest_desc(:,:,5) = Gx;
-steepest_desc(:,:,6) = Gy;
 
-% Hessian 
 H = zeros(6,6);
 for i = 1 : whole_rect(3)-whole_rect(1)+1
     for j = 1: whole_rect(4)-whole_rect(2)+1
+        % Steepest Descent
+        steepest_desc(:,:,i) = [Gx(j,i), Gy(j,i)].*init_dwdp(j,i);
         desc_pixel = reshape(steepest_desc(j,i,:),1,6);
+       
+         % Hessian 
         H = H + desc_pixel' * desc_pixel;
     end
 end
@@ -45,16 +38,25 @@ p6 = 0; %v
 
 epsilon = 0.01;
 p = [1+p1 p3 p5; p2 1+p4 p6];
-temp_It = imtranslate(It,[whole_rect(1)-rect(1),whole_rect(2)-rect(2)]);
-temp_It = temp_It(whole_rect(2):whole_rect(4), whole_rect(1):whole_rect(3));
 dP = p;
 newP = p;
 
 while norm(dP) > epsilon
-    % Warp image according to warped window
-    warp_It1 = imtranslate(It1,[-newP(5),-newP(6)]);
-    warp_It1 = warp_It1(whole_rect(2):whole_rect(4), whole_rect(1):whole_rect(3));
-    % Compute error by subtracting It1 window with I;t window
+    % Warp image according to transform matrix
+    tform = affine2d([p;[0,0,1]]');
+    warp_It1 = imwarp(It1,tform);
+    It1_mask = (warp_It1~=0);
+    % Compute error by substracting the overlap region
+    max_width = max(size(It1_mask,1),size(It_mask,1));
+    max_height = max(size(It1_mask,2),size(It_mask,2));
+    It1_common = zeros(max_height,max_width);
+    It1_common(1:size(It1_mask,2),1:size(It1_mask,1)) = It1_mask;
+    It_common = zeros(max_height,max_width);
+    It_common(1:size(It_mask,2),1:size(It_mask,1)) = It_mask;
+    
+    error_image = It1_common+It_common;
+    error_image = (error_image==2);
+    
     error_image = warp_It1 - temp_It;
     
     % Per pixel, multiply steepest descent
@@ -68,27 +70,23 @@ while norm(dP) > epsilon
     % Compute delta p
     dP = pinv(H)*SDparam;
     
-%     % Update the warp
-%     p1 = p1+dP(1)+p1*dP(1)+p3*dP(2);
-%     p2 = p2+dP(2)+p2*dP(1)+p4*dP(2);
-%     p3 = p3+dP(3)+p1*dP(3)+p3*dP(4);
-%     p4 = p4+dP(4)+p2*dP(3)+p4*dP(4);
-    p5 = p5+dP(5)+p1*dP(5)+p3*dP(6);
-    p6 = p6+dP(6)+p2*dP(5)+p4*dP(6);
-    newP = 1/((1+p1)*(1+p4)-p2*p3)*[
-%                                         -p1-p1*p4+p2*p3; ...
-%                                             -p2;...
-%                                             -p3;...
-%                                           -p4-p1*p4+p2*p3;...
-                                        0;
-                                        0;
-                                        0;
-                                        0;
-                                          -p5-p4*p5+p3*p6;...
-                                          -p6-p1*p6+p2*p5];
-    u = newP(5);
-    v = newP(6);
+    % Update the warp
+    newP = 1/((1+dP(1))*(1+dP(4))-p2*p3)*[-dP1-dP1*p4+p2*p3; ...
+                                    -p2;...
+                                    -p3;...
+                                    -p4-p1*p4+p2*p3;...
+                                    -p5-p4*p5+p3*p6;...
+                                    -p6-p1*p6+p2*p5];
+    
+    p1 = p1+newP(1)+p1*newP(1)+p3*newP(2);
+    p2 = p2+newP(2)+p2*newP(1)+p4*newP(2);
+    p3 = p3+newP(3)+p1*newP(3)+p3*newP(4);
+    p4 = p4+newP(4)+p2*newP(3)+p4*newP(4);
+    p5 = p5+newP(5)+p1*newP(5)+p3*newP(6);
+    p6 = p6+newP(6)+p2*newP(5)+p4*newP(6);
     
 end
 
-
+M = [1+newP(1) newP(3) newP(5);...
+     newP(2) 1+newP(4) newP(6);...
+     0       0         1     ];
